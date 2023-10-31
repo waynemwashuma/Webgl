19f28b20e64a9d7c17398d8d1e8e5b007151d441
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-01 00:30:13 +0300

    [Attributes] Created new base class to implement attributes #new

---
+++ attributes/attribute.js
@@ -0,0 +1,15 @@
+export class Attribute{
+  /**
+   * @readonly
+  */
+  value = null
+  buffer = null
+  count = 0
+  size = 0
+  location = null
+  constructor(value = [],size){
+    this.value = value
+    this.size = size
+    this.count = value.length/size
+  }
+}
