bcddb6ccfaded56f742c1951c9a6d6442f9f8dbd
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-10-31 23:46:31 +0300

    [Geometry] Refactor to fit new BufferGeometry #refactor

--- geometry/boxgeometry.js
+++ geometry/boxgeometry.js
@@ -2,6 +2,7 @@ import { Geometry } from "./geometry.js"
 
 export class BoxGeometry extends Geometry {
   constructor(w = 1, h = 1, l = 1) {
+    super()
     let vertices = [
 		// Top
 		-0.5 * w, 0.5 * h, -0.5 * l,
@@ -67,30 +68,32 @@ export class BoxGeometry extends Geometry {
 	  ]
 
     let indices = [
-		// Top
-		0, 1, 2,
-		0, 2, 3,
+      // Top
+      0, 1, 2,
+      0, 2, 3,
 
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
-	];
-    super(vertices, indices)
+      // Left
+      5, 4, 6,
+      6, 4, 7,
+      
+      // Right
+      8, 9, 10,
+      8, 10, 11,
+      
+      // Front
+      13, 12, 14,
+      15, 14, 12,
+      
+      // Back
+      16, 17, 18,
+      16, 18, 19,
+      
+      // Bottom
+      21, 20, 22,
+      22, 20, 23
+		]
+    this.setAttribute("indices", indices)
+    this.setAttribute("position", vertices)
+    this.setAttribute("uv", uvs)
   }
 }
--- geometry/planegeometry.js
+++ geometry/planegeometry.js
@@ -2,21 +2,25 @@ import { Geometry } from "./geometry.js"
 
 export class PlaneGeometry extends Geometry {
   constructor(w, h) {
+    super()
     let indices = [
-      0, 1, 2, 0, 3, 2, //bottom face
+      0, 1, 2,
+      0, 3, 2
       ]
     let vertices = [
-     0.5, 0.5, 0,
-     0.5, -0.5, 0,
-     -0.5, -0.5, 0,
-     -0.5, 0.5, 0,
+      0.5 * w, 0.5 * h, 0,
+      0.5 * w, -0.5 * h, 0,
+      -0.5 * w, -0.5 * h, 0,
+      -0.5 * w, 0.5 * h, 0
      ]
-     let uv = [
-       1,0,
-       1,1,
-       0,1,
-       0,0
+    let uv = [
+       1, 0,
+       1, 1,
+       0, 1,
+       0, 0
        ]
-    super(vertices, indices,null,uv)
+    this.setAttribute("indices", indices)
+    this.setAttribute("position", vertices)
+    this.setAttribute("uv", uv)
   }
 }
