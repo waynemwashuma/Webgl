309049c2cff4b06e3a6c04d9a6206dcd64bc30cf
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-06 00:27:58 +0300

    [Material] Main texture is now drawn on the basicMaerial. #fix

--- material/basicmaterial.js
+++ material/basicmaterial.js
@@ -2,33 +2,37 @@ import { Shader } from "./shader.js"
 import { Color } from "../Math/index.js"
 
 let vshader =
-  `precision mediump float;
+  `#version 300 es
+  precision mediump float;
 
-attribute vec3 position;
-attribute vec2 uv;
-attribute vec3 normal;
+in vec3 position;
+in vec2 uv;
+in vec3 normal;
 
-uniform mat4 uCamera;
-uniform mat4 uProjection;
-uniform mat4 uModel;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 model;
 
-varying vec2 v_uv;
+out vec2 v_uv;
 
 void main(){
-  gl_Position = uProjection *uCamera * uModel * vec4(position,1.0);
+  gl_Position = projection * view * model * vec4(position,1.0);
   v_uv = uv;
 }
 `
 let fshader =
-  `precision mediump float;
-
-uniform sampler2D texture;
-uniform vec4 color;
-
-varying vec2 v_uv;
-
-void main(){
-   gl_FragColor = texture2D(texture,v_uv) + color;
+  `#version 300 es
+  precision mediump float;
+  
+  uniform sampler2D mainTexture;
+  uniform vec4 color;
+  
+  in vec2 v_uv;
+  
+  out vec4 finalColor;
+  void main(){
+    finalColor = texture(mainTexture,v_uv);
+    //finalColor = vec4(v_uv,0.0,1.0);
 }
 `
 
@@ -36,18 +40,18 @@ export class BasicMaterial extends Shader {
   color = new Color()
   texture = null
   constructor(options) {
-    super()
-    this.vSrc = vshader
-    this.fSrc = fshader
-
     let {
-      color = new Color(255, 255, 255),
+      color = new Color(1, 1, 1),
         texture = null,
     } = options
+    
+    super(vshader,fshader)
+    
     this.color.copy(color)
     this.texture = texture
+    console.log(texture);
 
-    if (texture) this.setUniform("texture", texture)
+    if (texture) this.setUniform("mainTexture", texture)
     this.setUniform("color", this.color)
   }
 }
