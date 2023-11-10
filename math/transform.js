4947684a49f393c2a81b6cc8b6621e286b424e2e
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-10 11:42:36 +0300

    [Math] Transform supports hierarchy for scene graphs #change

--- math/transform.js
+++ math/transform.js
@@ -1,19 +1,43 @@
 import { Mat4 } from "/math/Mat4.js"
-import { Vec3 } from "/math/vec3.js"
+import { Vec3 as Vector3 } from "/math/vec3.js"
 
+let _position = new Vector3()
+let _rotation = new Vector3()
+let _scale = new Vector3()
 
-export class Transform{
-  constructor(){
+export class Transform {
+  constructor() {
     this.matrix = new Mat4()
-    this.position = new Vec3()
-    this.rotation = new Vec3()
-    this.scale = new Vec3(1,1,1)
-    this.offsetPosition = new Vector3()
-    this.offsetRotate = new Vector3()
-    this.offsetScale = new Vector3(1,1,1)
-    this.parent = null
+    this.position = new Vector3()
+    this.rotation = new Vector3()
+    this.scale = new Vector3(1, 1, 1)
   }
-  updateMatrix(){
-    this.matrix.compose(this.position,this.rotation,this.scale)
+  updateMatrix(parent) {
+    if (parent === void 0) {
+      this.matrix.compose(
+        this.position,
+        this.rotation,
+        this.scale
+      )
+      return
+    }
+    _scale
+      .copy(this.scale)
+      .multiply(parent.scale)
+    _rotation
+      .copy(this.rotation)
+      .add(parent.rotation)
+    _position
+      .copy(this.position)
+      .multiply(parent.scale)
+      .rotate(parent.rotation)
+      .add(parent.position)
+      .
+
+    this.matrix.compose(
+      _position,
+      _rotation,
+      _scale
+    )
   }
 }
