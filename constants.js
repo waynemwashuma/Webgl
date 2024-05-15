e1df07d65fc9e0855aa11d90716e356bf0562f33
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-16 01:38:29 +0300

    Refactored constants to remove unnecessary webgl context creation

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 export const ATTR_POSITION_NAME = "position"
 export const ATTR_NORMAL_NAME = "normal"
 export const ATTR_UV_NAME = "uv"
-export let ATTR_POSITION_LOC = 0
-export let ATTR_NORMAL_LOC = 1
-export let ATTR_UV_LOC = 2
+export const ATTR_POSITION_LOC = 0
+export const ATTR_NORMAL_LOC = 1
+export const ATTR_UV_LOC = 2
 export const UNI_PROJ_MAT = "projection"
 export const UNI_CAM_MAT = "view"
 export const UNI_MODEL_MAT = "model"
@@ -12,7 +12,7 @@ export const UNI_MODEL_MAT = "model"
  * @readonly
  * @enum {number}
  */
-export const UniformTypes = {
+export const UniformType = {
   BOOL: 0,
   FLOAT: 1,
   INT: 2,
@@ -34,103 +34,120 @@ export const UniformTypes = {
   ARR_BOOL: 18,
   STRUCT: 19
 }
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const GlDataType = {
+  FLOAT: 0x1406,
+  UNSIGNED_INT: 0x1405,
+  INT: 0x1404,
+  UNSIGNED_SHORT: 0x1403,
+  SHORT: 0x1402,
+  UNSIGNED_BYTE: 0x1401,
+  BYTE: 0x1400
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const DrawMode = {
+  POINTS: 0x0000,
+  LINES: 0x0001,
+  LINE_LOOP: 0x0002,
+  LINE_STRIP: 0x0003,
+  TRIANGLES: 0x0004,
+  TRIANGLE_STRIP: 0x0005,
+  TRIANGLE_FAN: 0x0006
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const DrawUsage = {
+  STATIC: 0x88E4,
+  DYNAMIC: 0x88E8,
+  STREAM: 0x88E0
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const CullFace = {
+  FRONT: 0x0404,
+  BACK: 0x0405,
+  BOTH: 0x0408
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const BlendMode = {
+  ZERO: 0x0000,
+  ONE: 0x00001,
+  SRC_COLOR: 0x0300,
+  ONE_MINUS_SRC_COLOR: 0x0301,
+  SRC_ALPHA: 0x0302,
+  ONE_MINUS_SRC_ALPHA: 0x0303,
+  DST_ALPHA: 0x0304,
+  ONE_MINUS_DIST_ALPHA: 0x0305,
+  DST_COLOR: 0x0306,
+  ONE_MINUS_DST_COLOR: 0x0307,
+  SRC_ALPHA_SATURATE: 0x0308,
+  CONSTANT_COLOR: 0x8001,
+  ONE_MINUS_CONSTANT_COLOR: 0x8002,
+  CONSTANT_ALPHA: 0x8003,
+  ONE_MINUS_CONSTANT_ALPHA: 0x8004
+}
 
-export const {
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  DrawModes,
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  CullFace,
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  BlendEquations,
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  TextureWrap,
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  TextureFormat,
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  TextureFilter,
-  /**
-   * @readonly
-   * @enum {number}
-   */
-  GlDataType,
-} = (
-  function() {
-    let canvas = document.createElement('canvas')
-    let gl = canvas.getContext("webgl2")
-
-    return {
-      DrawModes: {
-        POINTS: gl.POINTS,
-        TRIANGLES: gl.TRIANGLES,
-        TRIANGLE_FAN: gl.TRIANGLE_FAN,
-        TRIANGLE_STRIP: gl.TRIANGLE_STRIP,
-        LINES: gl.LINES,
-        LINE_LOOP: gl.LINE_LOOP,
-        LINE_STRIP: gl.LINE_STRIP
-      },
-      CullFace: {
-        FRONT: gl.FRONT,
-        BACK: gl.BACK,
-        BOTH: gl.FRONT_AND_BACK,
-        NONE: gl.NONE
-      },
-      AlphaMode: {
-
-      },
-      BlendEquations: {
-        SRC: gl.SRC_ALPHA,
-        DST: gl.DST_ALPHA,
-        ONE_MINUS_SRC: gl.ONE_MINUS_SRC_ALPHA,
-        ONE_MINUS_DIST: gl.ONE_MINUS_DST_ALPHA
-      },
-      GlDataType: {
-        INT: gl.INT,
-        UNSIGNED_INT: gl.UNSIGNED_INT,
-        FLOAT: gl.FLOAT,
-        UNSIGNED_SHORT: gl.UNSIGNED_SHORT,
-        SHORT: gl.SHORT,
-        UNSIGNED_BYTE: gl.UNSIGNED_BYTE,
-        BYTE: gl.BYTE
-      },
-      TextureFormat: {
-        RGBA: gl.RGBA,
-        RGBA16F: gl.RGBA16F,
-        RGBA32F: gl.RGBA32F,
-        RGBA8I: gl.RGBA8I,
-        RGBA16I: gl.RGBA16I,
-        RGBA32I: gl.RGBA32I
-      },
-      TextureWrap: {
-        REPEAT: gl.REPEAT,
-        CLAMP: gl.CLAMP_TO_EDGE,
-        MIRRORREPEAT: gl.MIRRORED_REPEAT
-      },
-      TextureFilter: {
-        NEAREST: gl.NEAREST,
-        LINEAR: gl.LINEAR,
-        NEAREST_MIPMAP_LINEAR: gl.NEAREST_MIPMAP_LINEAR,
-        NEAREST_MIPMAP_NEAREST: gl.NEAREST_MIPMAP_NEAREST,
-        NEAREST_MIPMAP_NEAREST: gl.NEAREST_MIPMAP_NEAREST,
-        LINEAR_MIPMAP_LINEAR: gl.LINEAR_MIPMAP_LINEAR,
-        LINEAR_MIPMAP_NEAREST: gl.LINEAR_MIPMAP_NEAREST,
-      }
-    }
-  })()
+export const BlendEquations = {
+  ADD: 0x8006,
+  SUBTRACT: 0x800A,
+  REVERSE_SUBTRACT: 0x800B
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const TextureFormat = {
+  DEPTH: 0x1902, //gl.DEPTH_COMPONENT
+  RED: 0x1903,
+  ALPHA: 0x1906,
+  RGB: 0x1907,
+  RGBA: 0x1908,
+  LUMINANCE: 0x1909,
+  LUMINANCE_ALPHA: 0x190A,
+  RGB8: 0x8051,
+  RGBA8: 0x8058,
+  RGBA16F: 0x881A,
+  RGB16F: 0x881B,
+  RGBA32F: 0x8814,
+  RGB32F: 0x8815,
+  RGBA32I: 0x8D82,
+  RGB32I: 0x8D83,
+  RGBA16I: 0x8D88,
+  RGB16I: 0x8D89,
+  RGBA8I: 0x8D8E,
+  RGB8I: 0x8D8F,
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const TextureFilter = {
+  NEAREST: 0x2600,
+  LINEAR: 0x2601,
+  NEAREST_MIPMAP_NEAREST: 0x2700,
+  LINEAR_MIPMAP_NEAREST: 0x2701,
+  NEAREST_MIPMAP_LINEAR: 0x2702,
+  LINEAR_MIPMAP_LINEAR: 0x2703,
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const TextureWrap = {
+  REPEAT: 0x2901,
+  CLAMP: 0x812F,
+  MIRRORREPEAT: 0x8370
+}
